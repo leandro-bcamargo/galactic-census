e7c44dd8b472f567d783c57334f566fc15e608e3
Leandro Camargo <leandro.bcamargo@gmail.com>
2024-03-30 10:03:51 -0300

    Add feature to display planet data upon click planet button

--- script.js
+++ script.js
@@ -9,12 +9,26 @@ async function displayPlanets() {
   const planets = await fetchPlanets();
   planets.forEach(planet => {
     const planetList = document.querySelector('.planet-list');
-    const listItem = document.createElement('li');
+    const planetListItem = document.createElement('li');
     const button = document.createElement('button');
     button.innerHTML = planet.name;
-    listItem.appendChild(button);
-    planetList.appendChild(listItem);
+    button.onclick = () => displayPlanetInfo(planet, planetListItem);
+    planetListItem.appendChild(button);
+    planetList.appendChild(planetListItem);
   })
+}
+
+function displayPlanetInfo({name, climate, population, terrain}, planetListItem) {
+  const planetInfoList = document.createElement('ul');
+  const planetInfoItems = 
+  `
+    <li>Name: ${name}</li>
+    <li>Climate: ${climate}</li>
+    <li>Population: ${population}</li>
+    <li>Terrain: ${terrain}</li>
+  `
+  planetInfoList.innerHTML = planetInfoItems;
+  planetListItem.append(planetInfoList);
 }
 
 
