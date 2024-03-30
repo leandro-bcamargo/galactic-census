0eb2b612af5eeb8efdfeffe2b75e565ce5ea2921
Leandro Camargo <leandro.bcamargo@gmail.com>
2024-03-29 23:53:36 -0300

    Create button list with planet names

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ async function fetchPlanets() {
   const data = await fetch('https://swapi.dev/api/planets/');
   const planets = await data.json();
 
-  console.log(planets.results);
+  return planets.results;
 }
+
+async function displayPlanets() {
+  const planets = await fetchPlanets();
+  planets.forEach(planet => {
+    const planetList = document.querySelector('.planet-list');
+    const listItem = document.createElement('li');
+    const button = document.createElement('button');
+    button.innerHTML = planet.name;
+    listItem.appendChild(button);
+    planetList.appendChild(listItem);
+  })
+}
+
 
